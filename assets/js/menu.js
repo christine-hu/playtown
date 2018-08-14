f6ad0434b9bfbeff4a02913d6e849da263e25d8e
Christine Hu <33097327+christine-hu@users.noreply.github.com>
2018-08-13 19:54:11 -0700

    fixing conflicts and style

--- assets/js/menu.js
+++ assets/js/menu.js
@@ -2,7 +2,7 @@ var menuState = {
 
 	create: function() {
 		// determining which control to listen for 
-	    if (selectedControl === 0) { 
+	    	if (selectedControl === 0) { 
 			control = enter;
 		} else if (selectedControl === 1) {
 			control = space;
@@ -55,16 +55,16 @@ var menuState = {
         	wordWrapWidth: 700, 
         	boundsAlignH: 'center', 
         	boundsAlignV: 'middle'
-        };
+       		};
 
-        // text 
-        p1 = game.add.text(0, 0, "Play or set preferences!", pStyle);
-        p1.setTextBounds(100, 185, 700, 165);
+		// text 
+		p1 = game.add.text(0, 0, "Play or set preferences!", pStyle);
+		p1.setTextBounds(100, 185, 700, 165);
 	
 		// title
 		game.add.sprite(159, 70, 'title');
 
-        // menu + back button sprites
+        	// menu + back button sprites
 		mainMenuSprite = game.add.sprite(86, 320, 'mainMenu');
 		textMenuSprite = game.add.sprite(86, 320, 'textMenu');
 		textMenuSprite.visible = false; 
@@ -73,25 +73,25 @@ var menuState = {
 		backButtonSprite = game.add.sprite(730, 590, 'backButton');
 		backButtonSprite.visible = false;
 		carrots = game.add.sprite(180, 540, 'carrots', 1);
-	    carrots.visible = false;
+	    	carrots.visible = false;
 
 		// animations (do not create if using two switches)
 	  	if (!twoSwitches) {
 		  	mainMenuSprite.animations.add('scroll');
-		    mainAnim = mainMenuSprite.animations.play('scroll', speed, true);
+		   	mainAnim = mainMenuSprite.animations.play('scroll', speed, true);
 
 		 	textMenuSprite.animations.add('scroll');
 			textAnim = textMenuSprite.animations.getAnimation('scroll');
 			
 			speedMenuSprite.animations.add('scroll');
-		    speedAnim = speedMenuSprite.animations.getAnimation('scroll');
+		    	speedAnim = speedMenuSprite.animations.getAnimation('scroll');
 
 			backButtonSprite.animations.add('press', [0, 0, 0, 1]);
 			backButtonAnim = backButtonSprite.animations.getAnimation('press');
 		}
 
 		// fade effect image 
-        black = game.add.sprite(0, 0, 'black');
+        	black = game.add.sprite(0, 0, 'black');
 
 		// escape to previous state
 		escape.onUp.add(prevState, this);
@@ -175,44 +175,43 @@ var menuState = {
 	    		}
     		}	
 
-    		// choosing a text size 
+    			// choosing a text size 
 			if (textSelect) {
 				// have to use pStyle instead of setting p1.fontSize for immediate update
 				if (textMenuSprite.frame == 0) {
 					textSize = '30px';
 					pStyle.fontSize = '30px';
-			        textSelection = 0;
-    			}
-    			else if (textMenuSprite.frame == 1) {
-    				textSize = '45px';
-    				pStyle.fontSize = '45px';
-			        textSelection = 1;
-    			}
-    			else if (textMenuSprite.frame == 2) {
-    				textSize = '60px';
-    				pStyle.fontSize = '60px';
-			        textSelection = 2;
-    			}
-    			p1.setStyle(pStyle, true);
-    			carrots.loadTexture('carrots', textSelection);
-    			if (backButtonSprite.frame == 1) {
-    				textMenuSprite.visible = false;
-    				backButtonSprite.visible = false;
-    				carrots.visible = false;
-    				p1.setText("Play or set preferences!", true);
-    				mainMenuSprite.visible = true;
-    				if (!twoSwitches) { 
-    					mainMenuSprite.animations.currentAnim.restart();
+			        	textSelection = 0;
+    				} else if (textMenuSprite.frame == 1) {
+					textSize = '45px';
+					pStyle.fontSize = '45px';
+					textSelection = 1;
+				} else if (textMenuSprite.frame == 2) {
+					textSize = '60px';
+					pStyle.fontSize = '60px';
+					textSelection = 2;
     				}
-    				textSelect = false;
-    				textMenu = false;
-    				mainMenu = true;
-    			}
+    				p1.setStyle(pStyle, true);
+    				carrots.loadTexture('carrots', textSelection);
+    				
+				if (backButtonSprite.frame == 1) {
+    					textMenuSprite.visible = false;
+    					backButtonSprite.visible = false;
+    					carrots.visible = false;
+    					p1.setText("Play or set preferences!", true);
+    					mainMenuSprite.visible = true;
+    					if (!twoSwitches) { 
+    						mainMenuSprite.animations.currentAnim.restart();
+    					}
+    					textSelect = false;
+    					textMenu = false;
+    					mainMenu = true;
+    				}
 			}
-
-    		if (backButtonSprite.frame != 1) {
-    			textSelect = true;
-    		}
+			
+			if (backButtonSprite.frame != 1) {
+				textSelect = true;
+			}
 		}
 
 		// speed preference screen 
@@ -221,21 +220,21 @@ var menuState = {
 			if (!speedSelect) {
 				p1.setText("Select a speed:", true);
 				p1.visible = true;
-	        	mainMenuSprite.visible = false;
-	        	speedMenuSprite.visible = true;
-	        	carrots.loadTexture('carrots', speedSelection);
-	        	carrots.visible = true;
-	    		backButtonSprite.visible = true;
-	    		if (!twoSwitches) {
-	    			speedAnim.play(speed, true);
-	    			backButtonAnim.play(speed, true);
-	    		} else {
-	    			speedMenuSprite.frame = speedSelection;
-	    			backButtonSprite.frame = 0; 
-	    		}
-    		}	
+	        		mainMenuSprite.visible = false;
+	        		speedMenuSprite.visible = true;
+	        		carrots.loadTexture('carrots', speedSelection);
+	        		carrots.visible = true;
+	    			backButtonSprite.visible = true;
+	    			if (!twoSwitches) {
+	    				speedAnim.play(speed, true);
+	    				backButtonAnim.play(speed, true);
+	    			} else {
+	    				speedMenuSprite.frame = speedSelection;
+	    				backButtonSprite.frame = 0; 
+	    			}
+    			}	
 
-    		// choosing a text size 
+			// choosing a speed 
 			if (speedSelect) {
 				if (!twoSwitches) {
 					if (speedMenuSprite.frame == 0) {
@@ -244,47 +243,41 @@ var menuState = {
 						mainAnim.delay = 5000;
 						speed = 0.2;
 						speedSelection = 0; 
-	    			}
-	    			if (speedMenuSprite.frame == 1) {
-<<<<<<< HEAD
-	    				speedAnim.delay = 2000;
-						backButtonAnim.delay = 2000;
-						mainAnim.delay = 2000;
-=======
-	    				speedAnim.delay = 3000;
+					}
+					if (speedMenuSprite.frame == 1) {
+						speedAnim.delay = 3000;
 						backButtonAnim.delay = 3000;
 						mainAnim.delay = 3000;
->>>>>>> gh-pages
-						speed = 0.5;
+						speed = 0.33;
 						speedSelection = 1;
-	    			}
-	    			if (speedMenuSprite.frame == 2) {
-	    				speedAnim.delay = 1000;
+					}
+					if (speedMenuSprite.frame == 2) {
+						speedAnim.delay = 1000;
 						backButtonAnim.delay = 1000;
 						mainAnim.delay = 1000;
 						speed = 1;
 						speedSelection = 2;
-	    			}
-    			}
-    			carrots.loadTexture('carrots', speedSelection);
-    			if (backButtonSprite.frame == 1) {
-    				speedMenuSprite.visible = false;
-    				backButtonSprite.visible = false;
-    				p1.setText("Play or set preferences!", true);
-    				carrots.visible = false;
-    				mainMenuSprite.visible = true;
-    				if (!twoSwitches) {
-    					mainMenuSprite.animations.currentAnim.restart();
-    				}
-    				speedSelect = false;
-    				speedMenu = false;
-    				mainMenu = true;
-    			}
+					}
+				}
+				carrots.loadTexture('carrots', speedSelection);
+				if (backButtonSprite.frame == 1) {
+					speedMenuSprite.visible = false;
+					backButtonSprite.visible = false;
+					p1.setText("Play or set preferences!", true);
+					carrots.visible = false;
+					mainMenuSprite.visible = true;
+					if (!twoSwitches) {
+						mainMenuSprite.animations.currentAnim.restart();
+					}
+					speedSelect = false;
+					speedMenu = false;
+					mainMenu = true;
+				}
 			}
 
 			if (backButtonSprite.frame != 1) {
-    			speedSelect = true;
-    		}
+    				speedSelect = true;
+    			}
 
 		}
 
@@ -295,7 +288,7 @@ var menuState = {
             fadeOut('map');
         } else {
 			fadeIn();
-        }
+       	}
 	},
 
 	shutdown: function() {
